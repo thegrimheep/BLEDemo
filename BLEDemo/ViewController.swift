5e4a486a006aef13f9c9e2365dd7d1e8de0540fe
David <davidporter33@icloud.com>
2018-08-23 11:39:03 -0700

    added notification settings but still not getting an alert

--- BLEDemo/ViewController.swift
+++ BLEDemo/ViewController.swift
@@ -8,21 +8,26 @@
 
 import UIKit
 import CoreLocation
+import UserNotifications
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    var locationManager: CLLocationManager
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options:[.alert, .sound]) { (granted, error) in }
+
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
     }
     // checking here to see if the use gave us permission or not then we scan
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedAlways{
+        if status == .authorizedAlways || status == .authorizedWhenInUse || status == .authorized {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
                     startScanning()
@@ -31,13 +36,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
 
+    // setting up the beacon entering the region
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        guard region is CLBeaconRegion else { return }
+
+        let content = UNMutableNotificationContent()
+        content.title = "David Porter"
+        content.body = "Can you please help?"
+        content.sound = .default()
+
+        let request = UNNotificationRequest(identifier: "BLEDemo", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+
     //here we are scanning for the beacons uuid
     func startScanning() {
-        let uuid = UUID(uuidString: "fake UUID goes here")!
+        let uuid = UUID(uuidString: "9118606C-C0C4-4AF3-9E7E-B839FDA1AB19")!
         let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 143, minor: 456, identifier: "Samaritan")
 
         locationManager.startMonitoring(for: beaconRegion)
         locationManager.startRangingBeacons(in: beaconRegion)
     }
+
+
 }
+
 
